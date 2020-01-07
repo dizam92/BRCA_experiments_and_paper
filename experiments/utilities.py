1b32265d62f4b82745fd448ea93cbf911596bad9
dizam92 <mazido@yahoo.fr>
2020-01-07 16:38:05 -0500

    build the new groups in file:pathways_biogrid_groups.pck and biogrid_pathways.pck

--- experiments/utilities.py
+++ experiments/utilities.py
@@ -19,6 +19,7 @@ from collections import Counter
 goa_file = '/home/maoss2/PycharmProjects/BRCA_experiments_and_paper/datasets/datasets_repository/goa_human_isoform_valid.gaf'
 biogrid_file = '/home/maoss2/PycharmProjects/BRCA_experiments_and_paper/datasets/datasets_repository/BIOGRID-ORGANISM-Homo_sapiens-3.5.178.tab.txt'
 genesID_file = '/home/maoss2/PycharmProjects/BRCA_experiments_and_paper/datasets/datasets_repository/Results_genes.txt'
+mirna_dat_file = '/home/maoss2/PycharmProjects/BRCA_experiments_and_paper/datasets/datasets_repository/miRNA.dat'
 
 c1_file = '/home/maoss2/PycharmProjects/BRCA_experiments_and_paper/datasets/datasets_repository/c1.all.v6.1.symbols.gmt'
 c2_file = '/home/maoss2/PycharmProjects/BRCA_experiments_and_paper/datasets/datasets_repository/c2.all.v6.1.symbols.gmt'
@@ -954,6 +955,32 @@ def main_construct():
                                                  view))
 
 
+def load_mirna_dat(data_path=data_tn_new_label_unbalanced_cpg_rna_rna_iso_mirna, return_views='all'):
+    """
+    It's a great idea but it is not worth it though. We have much more of the miRNA not related 
+    to a cluster at all even though i was using mirDB. Leaving it here to have a  proof of work.
+    Utility fonction to read the miRNA dat file and to do the groups(pathways) building for them
+    
+    """
+    _, _, features_names, _ = load_data(data=data_path, return_views=return_views)
+    features_names = list(features_names)
+    mirna_features_names = [feature for feature in features_names if feature.startswith('hsa')]
+    fichier = open(mirna_dat_file, 'r')
+    lines = fichier.readlines()
+    lines_to_keep = [] 
+    for line in lines:
+        if  line.startswith('ID') or line.startswith('DR   RFAM'):
+            lines_to_keep.append(line.strip('\n'))
+    dict_mirna_features_to_cluster = {features: [] for feature in mirna_features_names}
+    for feature in mirna_features_names:
+        for idx, line in enumerate(lines_to_keep):
+            if line.find(feature) != -1:
+                if lines_to_keep[idx + 1].find(RFAM) != -1:
+                    valeur = lines_to_keep[idx + 1].split(';')[-1].strip('.')
+                    dict_mirna_features_to_cluster[feature].append(valeur)
+    return dict_mirna_features_to_cluster
+
+
 def load_go_idmapping():
     """
     Utility function to map go_ids
@@ -993,6 +1020,66 @@ def load_biogrid_network():
         G.nodes[node]["go_terms"] = go_terms[node]
     print("BioGRID: {} genes and {} interactions".format(G.number_of_nodes(), G.number_of_edges()))
     return G, edges
+
+
+def build_dictionnary_groups(data_path=data_tn_new_label_balanced_cpg_rna_rna_iso_mirna, return_views='all',
+                                      output_file_name=''):
+    """
+    Utility function to build pathway file of the groups to be loaded in LearnFromBiogridGroup
+    The pathway will be stored in pickle file
+    Args:
+        data_path: str, data path
+        return_views: str, correct view for the group
+        output_file_name: str, output file name
+    Return:
+        output_file_name.pck
+    """
+    graph, _ = load_biogrid_network()
+    _, _, features_names, _ = load_data(data=data_path, return_views=return_views)
+    features_names = list(features_names)
+    adjacency_matrix = np.asarray(list(graph.adjacency()))
+    nodes = np.asarray(list(graph.nodes))
+    dico_results = {feature: [] for feature in features_names}
+    # noeud + et toutes leurs interactions; 
+    # el[0] is always the node and list(el[1].keys()) the genes whose are interacting with the node
+    biogrid_pathways = [list(el[1].keys()) + [el[0]] for el in adjacency_matrix] # Obtention des pathways de biogrids
+    biogrid_pathways.append(['miRNA']) # biogrid_pathways[20170]
+    biogrid_pathways.append(['clinical View']) # biogrid_pathways[20171]
+    biogrid_pathways.append(['INEXISTANT PATHWAYS KNOWN']) # biogrid_pathways[20172]
+    # Biogrids rank from 1 to 20170 + 3: pathways
+    for feature in features_names:
+        if feature.find('_') != -1:
+            # I went step by step for the comprehension but remember the gene is always at the end of the feature so
+            # use the [-1] access
+            split_results = feature.split('_')
+            gene_cible = split_results[-1]
+            if gene_cible == 'INEXISTANT': # SI on a pas pu le classifier direct on le met dnas le group inexistant
+                dico_results[feature].append(biogrid_pathways[20172])
+            if gene_cible.find(';'): # Cas ou on a un feature lié à 2 ou plus genes (surtout pour les cpg)
+                gene_cibles = gene_cible.split(';')
+                for gene in gene_cibles: # Recupere chaque gene et on remplit le dico
+                    for idx, pathway_in_biogrid in enumerate(biogrid_pathways):
+                        if gene.lower() in pathway_in_biogrid:
+                            dico_results[feature].append('G_{}'.format(idx))
+            else: # Different du premier if du coup le feature est link à un seul gene et on remplit le dictionnaire de facon adéquate
+                for idx, pathway_in_biogrid in enumerate(biogrid_pathways):
+                    if gene_cible.lower() in pathway_in_biogrid:
+                        dico_results[feature].append('G_{}'.format(idx))
+        elif feature.find('|') != -1: # Here the gene is the 1st element always since it's directly the RNA view only
+            split_results = feature.split('|')
+            gene_cible = split_results[0].lower()
+            for idx, pathway_in_biogrid in enumerate(biogrid_pathways):
+                if gene_cible in pathway_in_biogrid:
+                    dico_results[feature].append('G_{}'.format(idx))
+        elif feature.startswith('hsa'):  # MiRNA View: faire le traitement directement
+            dico_results[feature].append(biogrid_pathways[20170])
+        else:
+            dico_results[feature].append(biogrid_pathways[20171])
+    
+    with open('biogrid_pathways_list.pck', 'wb') as f:
+        pickle.dump(biogrid_pathways, f)
+    with open(output_file_name + '.pck', 'wb') as f:
+        pickle.dump(dico_results, f)
 
 
 def construction_biogrid_pathway_file(data_path=data_tn_new_label_balanced_cpg_rna_rna_iso_mirna, return_views='all',
