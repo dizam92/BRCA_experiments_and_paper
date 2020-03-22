e15de039b1e47bf461fcf36b14219e11c6fcd85b
dizam92 <mazido@yahoo.fr>
2020-03-22 09:23:58 -0400

    code for group scm on multiview dataset done (to be run)

---
+++ dispatch_baptiste_group_exp.py
@@ -0,0 +1,69 @@
+import os
+import random
+import numpy as np
+from sklearn.model_selection import ParameterGrid
+from os.path import join, abspath, dirname, exists
+from os import makedirs
+from subprocess import call
+import datetime
+print(__file__)
+RESULTS_PATH = os.environ.get('', join(dirname(abspath(__file__)), "saving_repository"))
+EXPERIMENTS_PATH = os.environ.get('', join(dirname(abspath(__file__)), "experiments"))
+DATAREPOSITORY_PATH = os.environ.get('', join(dirname(abspath(__file__)), "datasets/datasets_repository"))
+PROJECT_ROOT = dirname(abspath(__file__))
+
+def launch_slurm_experiment_group(subsampling, nb_repetitions, pathway_file, update_method, c, inverse_prior_group,
+                                  experiment_file, experiment_name, time, dispatch_path):
+    exp_file = join(dispatch_path, experiment_name)
+    submission_script = ""
+    submission_script += f"#!/bin/bash\n"
+    submission_script += f"#SBATCH --nodes=1\n" 
+    submission_script += f"#SBATCH --ntasks-per-node=8\n" 
+    submission_script += f"#SBATCH --mem=128000M\n" 
+    submission_script += f"#SBATCH --account=rpp-corbeilj\n"
+    submission_script += f"#SBATCH --mail-user=mazid-abiodoun.osseni.1@ulaval.ca\n"
+    submission_script += f"#SBATCH --mail-type=BEGIN\n"
+    submission_script += f"#SBATCH --mail-type=END\n"
+    submission_script += f"#SBATCH --mail-type=FAIL\n"
+    submission_script += f"#SBATCH --time={time}:00:00\n" 
+    submission_script += f"#SBATCH --output={exp_file + '.out'}\n\n" 
+    submission_script += f"python {EXPERIMENTS_PATH}/{experiment_file} -subs {subsampling} -nb_r {nb_repetitions} -g_dict {pathway_file} -u_m {update_method} -c {c} -inverse_prior_group {inverse_prior_group} -exp_name {experiment_name}" 
+    
+    submission_path = exp_file + ".sh"
+    with open(submission_path, 'w') as out_file:
+        out_file.write(submission_script)
+        
+    # call(["sbatch", submission_path])
+    
+def main_group():
+    dictionaries_paths = [f"{DATAREPOSITORY_PATH}/pathways_multiview_groups.pck"]
+    update_method = ['inner_group', 'outer_group']
+    subsampling_state = [False, True]
+    random.seed(42)
+    c_list = np.round(np.linspace(0.1, 1, 10), 3)
+    inverse_prior_group_list = [False, True]
+    param_grid = {'subsampling': subsampling_state, 'update': update_method, 'c': c_list, 'inverse_prior_group': inverse_prior_group_list}
+    dispatch_path = join(RESULTS_PATH, "dispatch_baptiste_group_exp")
+    if not exists(dispatch_path): makedirs(dispatch_path)
+    for pathway_dict in dictionaries_paths:
+        name_pathway_file = pathway_dict.split('/')[-1].split('.')[0]
+        print(f"Launching {pathway_dict}")
+        for params in ParameterGrid(param_grid):
+            print(f"Launching {params}")
+            nb_repetitions = 5
+            exp_name = f"{params['subsampling']}__group_scm__" + f"{name_pathway_file}__" + f"{params['update']}__" + f"c{params['c']}__" + f"inverse_prior_group{params['inverse_prior_group']}__" + f"{nb_repetitions}"
+            launch_slurm_experiment_group(subsampling=params['subsampling'], 
+                                            nb_repetitions=nb_repetitions, 
+                                            pathway_file=pathway_dict, 
+                                            update_method=params['update'], 
+                                            c=params['c'],
+                                            inverse_prior_group=params['inverse_prior_group'],
+                                            experiment_file='run_groups_scm_multiview_data_experiments.py', 
+                                            experiment_name=exp_name, 
+                                            time='5', 
+                                            dispatch_path=dispatch_path)
+    print("### DONE ###") 
+
+       
+if __name__ == '__main__':
+    main_group()
---
+++ experiments/run_groups_scm_multiview_data_experiments.py
@@ -0,0 +1,220 @@
+# -*- coding: utf-8 -*-
+__author__ = 'maoss2'
+from experiments.utilities import *
+from os.path import join, abspath, dirname, exists
+from os import makedirs
+from collections import defaultdict
+from learners.pyscmGroup import GroupSCM
+from sklearn.model_selection import train_test_split, GridSearchCV, KFold
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import confusion_matrix
+import argparse
+from functools import partial
+import multiprocessing
+from multiprocessing import Pool
+import logging
+logging.getLogger('parso.python.diff').disabled = True
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+nb_jobs = 30
+cv_fold = KFold(n_splits=5, random_state=42)
+
+project_path = '/home/maoss2/project/maoss2/'
+saving_rep = '/home/maoss2/project/maoss2/'
+dataset_baptiste = f'{project_path}/lives_14view_EMF.hdf5'
+
+
+class LearnGroupMultiViewData(object):
+    def __init__(self, parameters, learner, saving_dict, saving_file="", subsampling=False, 
+                 rs=42, nb_jobs=nb_jobs, cv=cv_fold, data_path=dataset_baptiste):
+        """
+        Learning Class to learn experiment on TN dataset
+        Args:
+            parameters: dict, parameters for the right learner
+            learner: obj, a learner DT, RF or DecisionStumpSCM
+            saving_dict: dict, an empty dictionary to save the results to
+            saving_file: str, output file where to write the saving_dict
+            rs: int, the random seed
+            nb_jobs: int, number of cpu to run the class
+            cv: int of Kfold object, crossvalidation object
+            data_path: str, path to dataset
+        """
+        self.parameters = parameters
+        self.learner = learner
+        self.saving_file = saving_file
+        self.rs = rs
+        self.data_path = data_path
+        self.saving_dict = saving_dict
+        self.nb_jobs = nb_jobs
+        self.cv = cv
+        self.subsampling = subsampling
+        self.gs_clf = GridSearchCV(self.learner, param_grid=self.parameters, n_jobs=self.nb_jobs, cv=self.cv, verbose=1)
+
+    @staticmethod
+    def save_features_selected(classifier, parameters, features_names, dictionary):
+        dictionary['rules'].append(classifier.best_estimator_.get_stats())
+        dictionary['rules_str'].append([(el.__str__(), features_names[el.feature_idx]) for el in
+                                        dictionary['rules'][-1]['Binary_attributes']])
+        dictionary['groups_rules'].append(classifier.best_estimator_.groups_rules)
+        logger.info('{}'.format(dictionary['rules_str']))
+
+    def fit(self, x, y):
+        self.gs_clf.fit(x, y)
+
+    def predict(self, x):
+        return self.gs_clf.predict(x)
+
+    def learning(self, features_names, x_train, x_test, y_train, y_test, proteins_ids_train, proteins_ids_test):
+        self.gs_clf.fit(x_train, y_train)
+        pred = self.gs_clf.predict(x_test)
+        y_train_pred = self.gs_clf.predict(x_train)
+        train_metrics = get_metrics(y_test=y_train, predictions_binary=y_train_pred)
+        print(self.learner)
+        print('*' * 50)
+        print('Train metrics', train_metrics)
+        metrics = get_metrics(y_test=y_test, predictions_binary=pred)
+        print('Test metrics', metrics)
+        print()
+        cnf_matrix = confusion_matrix(y_test, pred)
+        print(cnf_matrix)
+        self.saving_dict['metrics'] = metrics
+        self.saving_dict['train_metrics'] = train_metrics
+        self.saving_dict['cv_results'] = self.gs_clf.cv_results_
+        self.saving_dict['best_params'] = self.gs_clf.best_params_
+        self.saving_dict['cnf_matrix'] = cnf_matrix
+        proteins_ids_train = proteins_ids_train.astype('U13')
+        proteins_ids_test = proteins_ids_test.astype('U13')
+        self.saving_dict['proteins_ids_train'] = proteins_ids_train
+        self.saving_dict['proteins_ids_test'] = proteins_ids_test
+        self.saving_dict['importances'] = []
+        self.saving_dict['rules_str'] = []
+        self.saving_dict['rules'] = []
+        self.save_features_selected(classifier=self.gs_clf, parameters=self.parameters,
+                                    features_names=features_names, dictionary=self.saving_dict)
+        self.saving_file = self.saving_file + '_{}_{}.pck'.format(self.subsampling, str(self.rs))
+        with open(self.saving_file, 'wb') as f:
+            pickle.dump(self.saving_dict, f)
+            
+def f(c, x):
+    """Compute an update function"""
+    return np.sqrt(c * x)
+
+def f_1(c, x):
+    """Compute an update function"""
+    return np.exp( -c * x) 
+
+def build_priors_rules_vector(c,
+                              inverse_prior_group = False,
+                              dictionnary_for_prior_group=f"{data_repository}/multiview_pathways_dict.pck", 
+                              dictionnary_for_prior_rules=f"{data_repository}/pathways_multiview_groups.pck"):
+    """
+    Build the vector of the prior rules integreting the prior on the group/pathways 
+    Args:
+        c, an hp
+        dictionnary_for_prior_group, str, path to the dictionnary for generating . Structure must be: d = {'Group_name1': [gen1, gen100,...],  'Group_nameX': [genXXX,...]}
+        dictionnary_for_prior_rules, str, path to the dictionnary. Structure must be: d = {'Feature_name1': [Group_name1, Group_name100,...],  'Feature_nameX': [Group_nameXXX,...]}
+    Return:
+        prior_values_dict_pr_group, dict
+        prior_values_dict_pr_rules, dict
+    """   
+    dict_pr_group = pickle.load(open(dictionnary_for_prior_group, 'rb'))
+    dict_pr_rules = pickle.load(open(dictionnary_for_prior_rules, 'rb'))
+    # Build PriorGroups vector, p_g
+    prior_values_dict_pr_group = {k: f_1(c, len(v)) for k, v in dict_pr_group.items()} 
+    # Build PriorRules vector, p_ri
+    if inverse_prior_group:
+        prior_values_dict_pr_rules = {k: f_1(c, np.sum([1 / prior_values_dict_pr_group[el] for el in v])) for k, v in dict_pr_rules.items()}
+    else:
+        prior_values_dict_pr_rules = {k: f_1(c, np.sum([prior_values_dict_pr_group[el] for el in v])) for k, v in dict_pr_rules.items()}
+    return prior_values_dict_pr_group, prior_values_dict_pr_rules
+    
+    
+def run_experiment(pathway_file, nb_repetitions, update_method='inner_group', c=0.1, inverse_prior_group=False,
+                   subsampling=False, data=dataset_baptiste, experiment_name='experiment', saving_rep=saving_rep):
+    """
+    Utility function to run experiment on specific data and with specific wiew. To be called in a loop in a main
+    Args:
+        data: str, data path
+        update_method: str, name of the method to update the prioRules in the GroupSCM model
+                'inner_group': p_ri = p_ri * exp(| g_i \intersection GR | )  update 1
+                'outer_group': p_ri = p_ri * exp(-| g_i \intersection GR | )  update 2
+        c: float, hyperparameter for the prior rules and groups
+        pathway_file: str, path to the file containing information on the pathways
+        experiment_name: str, experiment name for saving file
+        return_views: str, which view to run experiment on
+        nb_repetitions: int, number of repetitions
+        saving_rep: str, saving repertory
+    Return:
+       Create a saving repertory and put the pickle results in
+    """
+    assert nb_repetitions >= 1, 'At least one split'
+    saving_dict_scm = defaultdict(dict)
+    _,_, _,_, _,_, _,_, _,_, _,_, _,_, _,_, _,_, _,_, _,_, _,_, _,_, _,_, x, groups_ids, y, proteins_ids, features_names = load_baptiste_data(dataset=dataset_baptiste, 
+                                                                                                                                     subsampling=subsampling) 
+    features_names = [el.encode("utf-8") for el in features_names]
+    features_names = [el.decode("utf-8") for el in features_names]
+    random.seed(42)
+    random_seeds_list = [random.randint(1, 2000) for _ in range(nb_repetitions)]
+    # Parameters for GROUP_SCM
+    dict_biogrid_groups = pickle.load(open(pathway_file, 'rb'))
+    features_to_index = {idx: name for idx, name in enumerate(features_names)}
+    _, prior_values_dict_pr_rules = build_priors_rules_vector(c=c, inverse_prior_group=inverse_prior_group)
+    prior_rules = [prior_values_dict_pr_rules[name] for name in features_names]
+    learner_clf = GroupSCM(features_to_index=features_to_index, 
+                           prior_rules=prior_rules, 
+                           update_method=update_method,
+                           groups=dict_biogrid_groups,
+                           tiebreaker='', 
+                           p=1.0, 
+                           model_type='conjunction', 
+                           max_rules=10)
+    results_repertory = join(saving_rep, experiment_name)
+    if not exists(results_repertory): makedirs(results_repertory)
+    os.chdir(f"{results_repertory}")
+    existing_files_list = [fichier for fichier in glob('*.pck')]
+    seeds_already_done = None
+    if len(existing_files_list) != 0:
+        seeds_already_done = [int(f.split('_')[-1].split('.')[0]) for f in existing_files_list]
+    for state in range(nb_repetitions):
+        if seeds_already_done is not None and random_seeds_list[state] in seeds_already_done: continue
+        clf = LearnGroupMultiViewData(parameters=parameters_group_scm,
+                            learner=learner_clf,
+                            saving_dict=saving_dict_scm,
+                            saving_file=experiment_name,
+                            rs=random_seeds_list[state],
+                            nb_jobs=nb_jobs,
+                            cv=cv_fold,
+                            data_path=data)
+        x_train, x_test, y_train, y_test, proteins_ids_train, proteins_ids_test = \
+            train_test_split(x, y, proteins_ids, train_size=0.8, random_state=random_seeds_list[state])
+        logger.info('Train set shape {}'.format(x_train.shape))
+        logger.info('Test set shape {}'.format(x_test.shape))
+        clf.learning(features_names=features_names, x_train=x_train, x_test=x_test,
+                        y_train=y_train, y_test=y_test, proteins_ids_train=proteins_ids_train,
+                        proteins_ids_test=proteins_ids_test)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Learn Group TN Experiment")
+    parser.add_argument('-nb_r', '--nb_repetitions', type=int, default=1)
+    parser.add_argument('-subs', '--subsampling', type=bool, default=False)
+    parser.add_argument('-g_dict', '--groups_dict', type=str, default=f"{data_repository}/pathways_multiview_groups.pck")
+    parser.add_argument('-u_m', '--update_method', type=str, default="neg_exp")
+    parser.add_argument('-c', '--c', type=float, default=0.1) 
+    parser.add_argument('-inverse_prior_group', '--inverse_prior_group', type=bool, default=False)
+    parser.add_argument('-data', '--data', type=str, default=dataset_baptiste)
+    parser.add_argument('-exp_name', '--experiment_name', type=str, default="experiment_group_scm")
+    parser.add_argument('-o', '--saving_rep', type=str, default=saving_rep)
+    args = parser.parse_args()
+    run_experiment(pathway_file=args.groups_dict, 
+                   subsampling=args.subsampling, 
+                   nb_repetitions=args.nb_repetitions,
+                   update_method=args.update_method, 
+                   c=args.c,
+                   inverse_prior_group=args.inverse_prior_group,
+                   data=args.data,  
+                   experiment_name=args.experiment_name, 
+                   saving_rep=args.saving_rep)    
+
+if __name__ == '__main__':
+    main()
